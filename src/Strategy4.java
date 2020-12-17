bf21ea2f9adfe85bb53f89bd4376b533e83a98a1
AnasFRANCIS <anas.francis@etu.univ-cotedazur.fr>
2020-12-17 13:52:40 +0100

    #1 boucle infinie ?

--- src/Strategy4.java
+++ src/Strategy4.java
@@ -18,22 +18,27 @@ public class Strategy4 implements Strategy {
     public void question3Algorithme(Graph graph) {
         List<Vertex> redVertex = Utility.getRedVertex(graph);
         while (atLeasOnLinkedVertex(redVertex)) { // s'il y a au moins un sommet rouge avec des arc entant/sortant, on continue
+            boolean atLeastOneCaseStillValid = false;
             for (Vertex v : redVertex) { // supprimer tous les rouges avec 0 sortants
                 if (v.nbArcSortant() == 0){
+                    atLeastOneCaseStillValid = true;
                     graph.deleteVertex(v.getId());
                 }
             }
             for (Vertex v : redVertex) { // supprimer tous les rouge avec 1 arc sortant bleu vers sommet bleu (il a uniquement un arc sortant)
                 if (v.nbArcSortant() == 1 && v.nbArcBleuSortantVersBleu == 1) { // conditions nécessaires suffisantes
+                    atLeastOneCaseStillValid = true;
                     graph.deleteVertex(v.getId());
                 }
             }
             for (Vertex v : redVertex) {
                 if (v.nbArcRougeSortant > 0) { // supprimer 1 seul rouge avec 1 ou 2 arcs rouge partant vers n'importe quelle vertex
+                    atLeastOneCaseStillValid = true;
                     graph.deleteVertex(v.getId());
                     break;
                 }
             }
+            if(!atLeastOneCaseStillValid) break; // nécessaire, sinon boucle infinie !
         }
         deleteAllRedVertices(graph); // peut-être pas nécessaire à cause la ligne 21 :-/
     }
